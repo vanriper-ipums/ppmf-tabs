852db1f24701e67237fe7857da3e611bcabec728
David Van Riper <vanriper@umn.edu>
2020-07-01 02:59:45 +0000

    Added colClass for character-based recodes

--- r_docs/2_load_recodes.r
+++ r_docs/2_load_recodes.r
@@ -6,6 +6,9 @@
 
 require(data.table)
 
+#### Col classes #### 
+recode_col_classes <- c("character", "integer")
+
 #### Read in age recodes #### 
 
 #Voting age recode (under 18 / over 18)
@@ -20,25 +23,25 @@ age12 <- fread("data/recodes/age_p12.csv")
 #### Read in race recodes ####
 
 # 7-category race recode 
-race7 <- fread("data/recodes/race7.csv")
+race7 <- fread("data/recodes/race7.csv", colClasses = recode_col_classes)
 
 # Race alone or in combination tally recode - this tells you the number of races recorded by the 
 # respondant 
-race_tally <- fread("data/recodes/raceTally.csv") 
+race_tally <- fread("data/recodes/raceTally.csv", colClasses = recode_col_classes) 
 
 #### Read in sex recode ####
 
 # Recode sex from character to integer value
-sex <- fread("data/recodes/sex.csv")
+sex <- fread("data/recodes/sex.csv", colClasses = recode_col_classes)
 
 #### Read in sex recode ####
 
 # Recode cenhisp from character to integer value
-hisp <- fread("data/recodes/cenhisp.csv")
+hisp <- fread("data/recodes/cenhisp.csv", colClasses = recode_col_classes)
 
 #### Read in rtype recode ####
 
 # Recode rtype from character to integer value
-sex <- fread("data/recodes/rtype.csv")
+rtype <- fread("data/recodes/rtype.csv", colClasses = recode_col_classes)
 
 
