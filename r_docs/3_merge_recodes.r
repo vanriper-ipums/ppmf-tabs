3b29635606a4b9113edf4dac0f37ab31d531a70a
David Van Riper <vanriper@umn.edu>
2020-07-01 17:08:44 +0000

    Init commit of 3_merge_recodes.r

---
+++ r_docs/3_merge_recodes.r
@@ -0,0 +1,26 @@
+# 3_merge_recodes.r 
+# Author: David Van Riper
+# Created: 2020-07-01
+# 
+# This script merges the recodes onto dt
+
+require(data.table)
+
+#### Set keys in dt and recodes ####
+setkey(dt, c("qsex", "cenhisp", "cenrace", "qage"))
+setkey(sex, "qsex")
+setkey(hisp, "cenhisp")
+setkey(race7, "cenrace")
+setkey(age12, "qage")
+
+#### Sex ####
+sex[dt]
+
+#### Hispanic ####
+hisp[dt]
+
+#### Race7 ####
+race7[dt]
+
+#### Age12 ####
+age12[dt]
