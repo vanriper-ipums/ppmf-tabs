56578320e01986aaddb3ddf210a513dee9dbc756
David Van Riper <vanriper@umn.edu>
2020-07-01 16:12:42 +0000

    Added hisp_race7 and sex_age_p12 headers

--- r_docs/4_load_column_headers.r
+++ r_docs/4_load_column_headers.r
@@ -10,4 +10,6 @@ require(tidyverse)
 header_hisp <- read_csv("data/headers/header_hisp.csv")
 header_sex <- read_csv("data/headers/header_sex.csv")
 header_race7 <- read_csv("data/headers/header_race7.csv")
+header_hisp_race7 <- read_csv("data/headers/header_hisp_race7.csv")
+header_sex_age12 <- read_csv("data/headers/header_sex_age_p12.csv")
 
