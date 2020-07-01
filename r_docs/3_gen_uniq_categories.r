de931ed875eb033d42a79e4de6daed17242a4d88
David Van Riper <vanriper@umn.edu>
2020-07-01 13:38:20 +0000

    Init commit of 3_gen_uniq_categories.r script

---
+++ r_docs/3_gen_uniq_categories.r
@@ -0,0 +1,51 @@
+# 3_gen_uniq_categories.r
+# Author: David Van Riper
+# Created: 2020-07-01
+# 
+# This script generates unique values for each recode dt.
+
+require(data.table)
+source("r_docs/functions.r")
+
+#### Create copies of each recode dt #### 
+uniq_voting <- voting
+uniq_age5 <- age5
+uniq_age12 <- age12
+uniq_sex <- sex
+uniq_race7 <- race7
+uniq_race_tally <- race_tally
+uniq_race63 <- race63
+uniq_race11 <- race11
+uniq_hisp <- hisp
+uniq_rtype <- rtype
+
+#### Remove original value from each dt ####
+uniq_voting[, qage := NULL] 
+uniq_age5[, qage := NULL]
+uniq_age12[, qage := NULL]
+uniq_sex[, qsex := NULL]
+uniq_race7[, cenrace := NULL]
+uniq_race_tally[, cenrace := NULL]
+uniq_race63[, cenrace := NULL]
+uniq_race11[, cenrace := NULL]
+uniq_hisp[, cenhisp := NULL]
+uniq_rtype[, rtype := NULL]
+
+#### Create unique values for each recode ####
+uniq_voting <- unique(uniq_voting)
+uniq_age5 <- unique(uniq_age5)
+uniq_age12 <- unique(uniq_age12)
+uniq_sex <- unique(uniq_sex)
+uniq_race7 <- unique(uniq_race7)
+uniq_race_tally <- unique(uniq_race_tally)
+uniq_race11 <- unique(uniq_race11)
+uniq_hisp <- unique(uniq_hisp)
+uniq_rtype <- unique(uniq_rtype)
+
+#### Cartesian joins ####
+sex_age12 <- cartesian_join(uniq_sex, uniq_age12)
+#voting_race63 <- cartesian_join(uniq_voting, uniq_race63)
+hisp_race7 <- cartesian_join(uniq_hisp, uniq_race7)
+hisp_race63 <- cartesian_join(uniq_hisp, uniq_race63)
+hisp_race11 <- cartesian_join(uniq_hisp, uniq_race11)
+
