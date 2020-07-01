331f7b261ec18dc3bd1df1c7135d88a27791f74d
David Van Riper <vanriper@umn.edu>
2020-07-01 17:10:28 +0000

    Init commit of 6_compute_dummy_vars.r

---
+++ r_docs/6_compute_dummy_vars.r
@@ -0,0 +1,35 @@
+# 6_compute_dummy_vars.r
+# Author: David Van Riper
+# Created: 2020-07-01
+# 
+# This script uses the header tibbles to compute values for the dummy variables in dt.
+
+require(data.table)
+
+#### Total population #### 
+# This dummy is easiast to fill in - all records get a 1
+dt[, P0010001 := 1]
+
+#### Hispanic/Not Hispanic #### 
+# For each value in header_hisp, set appropriate P var to 1 
+for(row in 1:nrow(header_hisp)){
+  dt[, header_hisp$header[row] := fifelse(hisp == header_hisp$hisp[row], 1, 0)]
+}
+
+#### Sex #### 
+# For each value in header_sex, set appropriate P var to 1 
+for(row in 1:nrow(header_sex)){
+  dt[, header_sex$header[row] := fifelse(sex == header_sex$sex[row], 1, 0)]
+}
+
+#### Race7 #### 
+# For each value in header_race7, set appropriate P var to 1 
+for(row in 1:nrow(header_race7)){
+  dt[, header_race7$header[row] := fifelse(race7 == header_race7$race7[row], 1, 0)]
+}
+
+#### Hispanic by Race7 ####
+# For each value in header_hisp_race7, set appropriate P var to 1
+for(row in 1:nrow(header_hisp_race7)){
+  dt[, header_hisp_race7$header[row] := fifelse((hisp == header_hisp_race7$hisp[row] & race7 == header_hisp_race7$race7[row]), 1, 0)]
+}
