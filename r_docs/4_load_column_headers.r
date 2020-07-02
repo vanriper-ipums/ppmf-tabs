bebd54c32aa5100d937352755f04958ef9fa2e56
David Van Riper <vanriper@umn.edu>
2020-07-02 02:54:25 +0000

    Use NHGIS var codes for column headers to avoide duplicates.

--- r_docs/4_load_column_headers.r
+++ r_docs/4_load_column_headers.r
@@ -7,15 +7,15 @@
 require(tidyverse)
 
 #### Load column headers #### 
-header_hisp <- read_csv("data/headers/header_hisp.csv")
-header_sex <- read_csv("data/headers/header_sex.csv")
-header_race7 <- read_csv("data/headers/header_race7.csv")
-#header_race63 <- read_csv("data/headers/header_hisp_race63.csv")
-#header_race63_voting_age <- read_csv("data/headers/header_race63_voting_age.csv")
-header_hisp_race7 <- read_csv("data/headers/header_hisp_race7.csv")
-#header_hisp_race63 <- read_csv("data/headers/header_hisp_race63.csv")
-#header_hisp_race63_voting_age <- read_csv("data/headers/header_hisp_race63_voting_age.csv")
-header_sex_age12 <- read_csv("data/headers/header_sex_age_p12.csv")
-#header_major_gqtype <- read_csv("data/headers/header_major_gqtype.csv")
+header_hisp <- read_csv("data/headers/header_nhgis_hisp.csv")
+header_sex <- read_csv("data/headers/header_nhgis_sex.csv")
+header_race7 <- read_csv("data/headers/header_nhgis_race7.csv")
+header_race63 <- read_csv("data/headers/header_nhgis_race63.csv")
+header_race63_voting_age <- read_csv("data/headers/header_nhgis_race63_voting_age.csv")
+header_hisp_race7 <- read_csv("data/headers/header_nhgis_hisp_race7.csv")
+header_hisp_race63 <- read_csv("data/headers/header_nhgis_hisp_race63.csv")
+header_hisp_race63_voting_age <- read_csv("data/headers/header_nhgis_hisp_race63_voting_age.csv")
+header_sex_age12 <- read_csv("data/headers/header_nhgis_sex_age_p12.csv")
+header_major_gqtype <- read_csv("data/headers/header_nhgis_major_gqtype.csv")
 
 
