006388213d7a74514f82187e3be25e103302a5ac
David Van Riper <vanriper@umn.edu>
2020-07-02 03:42:30 +0000

    Init commit of 7_sum_dummy_vars_by_geography.r

---
+++ r_docs/7_sum_dummy_vars_by_geography.r
@@ -0,0 +1,9 @@
+# 7_sum_dummy_vars_by_geography.r
+# Author: David Van Riper
+# Created: 2020-07-01
+# 
+# This script groups dt by geography vars and sums the dummy variable columns, giving me counts.
+
+require(data.table)
+
+#### 
