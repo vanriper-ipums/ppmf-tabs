babb49e0460d229d8a0f3c668a25a36ed193124d
David Van Riper <vanriper@umn.edu>
2020-07-02 13:08:30 +0000

    Added create dummies, calculate dummies, collapse on block and write out to CSV for each table.

--- r_docs/6_compute_dummy_vars.r
+++ r_docs/6_compute_dummy_vars.r
@@ -8,65 +8,200 @@ require(data.table)
 
 #### Total population #### 
 # This dummy is easiast to fill in - all records get a 1
-#dt[, P0010001 := 1]
 dt[, H7V001_dp := 1]
 
-#### Hispanic/Not Hispanic #### 
-# For each value in header_hisp, set appropriate P var to 1 
-for(row in 1:nrow(header_hisp)){
-  dt[, header_hisp$header[row] := fifelse(hisp == header_hisp$recode[row], 1, 0)]
-}
-
 #### Race7 #### 
+# Create vector with dummy var names 
+vars <- header_race7$header
+
+# Add dummies to dt
+dt[, (vars) := 0]
+
 # For each value in header_race7, set appropriate P var to 1 
 for(row in 1:nrow(header_race7)){
   dt[, header_race7$header[row] := fifelse(race7 == header_race7$recode[row], 1, 0)]
 }
 
+# Create block-level total pops
+block <- dt[, lapply(.SD, sum),
+            by = .(TABBLKST, TABBLKCOU, TABTRACTCE, TABBLK),
+            .SDcols = H7V001_dp:H7X008_dp]
+
+# Write out to CSV for further processing
+fwrite(block, file = "data/output/block_p1_p3.csv")
+
+# Set H7V001_dp and vars to null to remove from dt
+dt[, H7V001_dp := NULL]
+dt[, (vars) := NULL]
+
+#### Hispanic/Not Hispanic #### 
+# Create vector with dummy var names 
+vars <- header_hisp$header
+
+# Add dummies to dt
+dt[, (vars) := 0]
+
+# For each value in header_hisp, set appropriate P var to 1 
+for(row in 1:nrow(header_hisp)){
+  dt[, header_hisp$header[row] := fifelse(hisp == header_hisp$recode[row], 1, 0)]
+}
+
+# Create block-level total pops
+block <- dt[, lapply(.SD, sum),
+            by = .(TABBLKST, TABBLKCOU, TABTRACTCE, TABBLK),
+            .SDcols = H7Y002_dp:H7Y003_dp]
+
+# Write out to CSV for further processing
+fwrite(block, file = "data/output/block_p4.csv")
+
+# Set vars to null to remove from dt
+dt[, (vars) := NULL]
+
 #### Hispanic by Race7 ####
+# Create vector with dummy var names 
+vars <- header_hisp_race7$header
+
+# Add dummies to dt
+dt[, (vars) := 0]
+
 # For each value in header_hisp_race7, set appropriate P var to 1
 for(row in 1:nrow(header_hisp_race7)){
   dt[, header_hisp_race7$header[row] := fifelse((hisp == header_hisp_race7$hisp[row] & race7 == header_hisp_race7$race7[row]), 1, 0)]
 }
 
+# Create block-level total pops
+block <- dt[, lapply(.SD, sum),
+            by = .(TABBLKST, TABBLKCOU, TABTRACTCE, TABBLK),
+            .SDcols = H7Z003_dp:H7Z017_dp]
+
+# Write out to CSV for further processing
+fwrite(block, file = "data/output/block_p5.csv")
+
+# Set vars to null to remove from dt
+dt[, (vars) := NULL]
+
 #### Major GQ type #### 
+# Create vector with dummy var names 
+vars <- header_major_gqtype$header
+
+# Add dummies to dt
+dt[, (vars) := 0]
+
 # For each value in header_major_gqtype, set appropriate P var to 1
 for(row in 1:nrow(header_major_gqtype)){
   dt[, header_major_gqtype$header[row] := fifelse(gqtypen == header_major_gqtype$recode[row], 1, 0)]
 }
 
+# Write out to CSV for further processing
+fwrite(block, file = "data/output/block_p42.csv")
+
+# Set vars to null to remove from dt
+dt[, (vars) := NULL]
+
 #### Sex #### 
+# Create vector with dummy var names 
+vars <- header_sex$header
+
+# Add dummies to dt
+dt[, (vars) := 0]
+
 # For each value in header_sex, set appropriate P var to 1 
 for(row in 1:nrow(header_sex)){
   dt[, header_sex$header[row] := fifelse(sex == header_sex$recode[row], 1, 0)]
 }
 
+# Write out to CSV for further processing
+fwrite(block, file = "data/output/block_p12_sex.csv")
+
+# Set vars to null to remove from dt
+dt[, (vars) := NULL]
+
 #### Sex by Age_p12 #### 
+# Create vector with dummy var names 
+vars <- header_sex_age12$header
+
+# Add dummies to dt
+dt[, (vars) := 0]
+
 # For each value in header_sex_age12, set appropriate P var to 1
 for(row in 1:nrow(header_sex_age12)){
   dt[, header_sex_age12$header[row] := fifelse((sex == header_sex_age12$sex[row] & age_p12 == header_sex_age12$age_p12[row]), 1, 0)]
 }
 
-#### Race63 #### 
+# Write out to CSV for further processing
+fwrite(block, file = "data/output/block_p12.csv")
+
+# Set vars to null to remove from dt
+dt[, (vars) := NULL]
+
+#### Race63 ####
+# Create vector with dummy var names 
+vars <- header_race63$header
+
+# Add dummies to dt
+dt[, (vars) := 0] 
+
 # For each value in header_race63, set appropriate P var to 1
 for(row in 1:nrow(header_race63)){
   dt[, header_race63$header[row] := fifelse(race63 == header_race63$recode[row], 1, 0)]
 }
 
+# Write out to CSV for further processing
+fwrite(block, file = "data/output/block_p8.csv")
+
+# Set vars to null to remove from dt
+dt[, (vars) := NULL]
+
 #### Hispanic or Not Hispanice by Race63 #### 
+# Create vector with dummy var names 
+vars <- header_hisp_race63$header
+
+# Add dummies to dt
+dt[, (vars) := 0] 
+
 # For each value in header_hisp_race63, set appropriate P var to 1
 for(row in 1:nrow(header_hisp_race63)){
   dt[, header_hisp_race63$header[row] := fifelse((hisp == header_hisp_race63$hisp[row] & race63 == header_hisp_race63$race63[row]), 1, 0)]
 }
 
+# Write out to CSV for further processing
+fwrite(block, file = "data/output/block_p9.csv")
+
+# Set vars to null to remove from dt
+dt[, (vars) := NULL]
+
 #### Race63 by Voting age #### 
+# Create vector with dummy var names 
+vars <- header_race63_voting_age$header
+
+# Add dummies to dt
+dt[, (vars) := 0]
+
 # For each value in header_race63_voting_age, set appropriate P var to 1
 for(row in 1:nrow(header_race63_voting_age)){
   dt[, header_race63_voting_age$header[row] := fifelse((voting_age == header_race63_voting_age$voting_age[row] & race63 == header_hisp_race63_voting_age$race63[row]), 1, 0)]
 }
 
-#### Hispanic or Not Hispanice by Race63 by Voting age ####  
+# Write out to CSV for further processing
+fwrite(block, file = "data/output/block_p10.csv")
+
+# Set vars to null to remove from dt
+dt[, (vars) := NULL]
+
+#### Hispanic or Not Hispanice by Race63 by Voting age #### 
+# Create vector with dummy var names 
+vars <- header_hisp_race63_voting_age$header
+
+# Add dummies to dt
+dt[, (vars) := 0]
+
 # For each value in header_hisp_race63_voting_age, set appropriate P var to 1
 for(row in 1:nrow(header_hisp_race63_voting_age)){
   dt[, header_hisp_race63_voting_age$header[row] := fifelse((voting_age == header_hisp_race63_voting_age$voting_age[row] & hisp == header_hisp_race63_voting_age$hisp[row] & race63 == header_hisp_race63_voting_age$race63[row]), 1, 0)]
 }
+
+# Write out to CSV for further processing
+fwrite(block, file = "data/output/block_p11.csv")
+
+# Set vars to null to remove from dt
+dt[, (vars) := NULL]
