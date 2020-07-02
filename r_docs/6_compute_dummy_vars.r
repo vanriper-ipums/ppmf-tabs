b7101fd97dfaa617c154fcf8ea6a26b658ea23bb
David Van Riper <vanriper@umn.edu>
2020-07-02 11:22:10 +0000

    Rearranged the dummy calculations

--- r_docs/6_compute_dummy_vars.r
+++ r_docs/6_compute_dummy_vars.r
@@ -17,12 +17,6 @@ for(row in 1:nrow(header_hisp)){
   dt[, header_hisp$header[row] := fifelse(hisp == header_hisp$recode[row], 1, 0)]
 }
 
-#### Sex #### 
-# For each value in header_sex, set appropriate P var to 1 
-for(row in 1:nrow(header_sex)){
-  dt[, header_sex$header[row] := fifelse(sex == header_sex$recode[row], 1, 0)]
-}
-
 #### Race7 #### 
 # For each value in header_race7, set appropriate P var to 1 
 for(row in 1:nrow(header_race7)){
@@ -35,16 +29,22 @@ for(row in 1:nrow(header_hisp_race7)){
   dt[, header_hisp_race7$header[row] := fifelse((hisp == header_hisp_race7$hisp[row] & race7 == header_hisp_race7$race7[row]), 1, 0)]
 }
 
-#### Sex by Age_p12 #### 
-# For each value in header_sex_age12, set appropriate P var to 1
-for(row in 1:nrow(header_sex_age12)){
-  dt[, header_sex_age12$header[row] := fifelse((sex == header_sex_age12$sex[row] & age_p12 == header_sex_age12$age_p12[row]), 1, 0)]
-}
-
 #### Major GQ type #### 
 # For each value in header_major_gqtype, set appropriate P var to 1
 for(row in 1:nrow(header_major_gqtype)){
   dt[, header_major_gqtype$header[row] := fifelse(gqtypen == header_major_gqtype$recode[row], 1, 0)]
+}
+
+#### Sex #### 
+# For each value in header_sex, set appropriate P var to 1 
+for(row in 1:nrow(header_sex)){
+  dt[, header_sex$header[row] := fifelse(sex == header_sex$recode[row], 1, 0)]
+}
+
+#### Sex by Age_p12 #### 
+# For each value in header_sex_age12, set appropriate P var to 1
+for(row in 1:nrow(header_sex_age12)){
+  dt[, header_sex_age12$header[row] := fifelse((sex == header_sex_age12$sex[row] & age_p12 == header_sex_age12$age_p12[row]), 1, 0)]
 }
 
 #### Race63 #### 
